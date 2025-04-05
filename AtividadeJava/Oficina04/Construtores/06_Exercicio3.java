dcafdb65fb58c9dff68fc91c1600a4198cac8bde
Luiz Henrique <50057405+c4pluiz1@users.noreply.github.com>
2025-04-04 21:48:25 -0300

    Create 06_Exercicio3.java

---
+++ AtividadeJava/Oficina04/Construtores/06_Exercicio3.java
@@ -0,0 +1,9 @@
+public class Exercicio3 {
+    public static void main(String[] args) {
+        Produto p1 = new Produto("Caderno");
+        Produto p2 = new Produto("Mouse", 89.90, 10);
+ 
+        p1.exibir();
+        p2.exibir();
+    }
+}
