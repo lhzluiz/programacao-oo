1ece3da6978034a33233231a75c1119e9d62e38b
Luiz Henrique <50057405+c4pluiz1@users.noreply.github.com>
2025-04-04 21:40:50 -0300

    Update 01_Pessoa.java

--- AtividadeJava/Oficina04/Construtores/01_Pessoa.java
+++ AtividadeJava/Oficina04/Construtores/01_Pessoa.java
@@ -1,1 +1,16 @@
+public class Pessoa {
+    private String nome;
+    private int idade;
+   // Construtor que vai inicializar
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+ 
+     // Método 
+    public void exibir() {
+        System.out.printf("Nome da Pessoa: %s\n", nome);
+        System.out.printf("Idade: %d\n", idade);
+    }
+}
 
