6c536d13c7214f27c31be7331d266822e95fc99b
Luiz Henrique <50057405+c4pluiz1@users.noreply.github.com>
2025-04-04 21:45:38 -0300

    Create 04_Exercicio2.java

---
+++ AtividadeJava/Oficina04/Construtores/04_Exercicio2.java
@@ -0,0 +1,9 @@
+public class Exercicio2 {
+    public static void main(String[] args) {
+        Livro l1 = new Livro();
+        Livro l2 = new Livro("Dom Casmurro", "Machado de Assis", 1899);
+ 
+        l1.exibir();
+        l2.exibir();
+    }
+}
