30dee4d464fff83b0cb12bb177db9df821574156
Luiz Henrique <50057405+lhzluiz@users.noreply.github.com>
2025-05-14 17:02:54 -0300

    Create Arqueiro.java

---
+++ ed02/Jogo/Arqueiro.java
@@ -0,0 +1,10 @@
+class Arqueiro extends Personagem {
+    public Arqueiro(String nome) {
+        super(nome, 120, 20);
+    }
+
+    @Override
+    public void usarHabilidadeEspecial() {
+        System.out.println(nome + " dispara Chuva de Flechas!");
+    }
+}
