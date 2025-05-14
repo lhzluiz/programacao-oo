370305e2efce403247ddb66d494090a07ae267b8
Luiz Henrique <50057405+lhzluiz@users.noreply.github.com>
2025-05-14 17:03:18 -0300

    Create Mainrpg.java

---
+++ ed02/Jogo/Mainrpg.java
@@ -0,0 +1,19 @@
+class MainRpg {
+    public static void main(String[] args) {
+        Guerreiro g = new Guerreiro("Thorgal");
+        Mago m = new Mago("Merlino");
+        Arqueiro a = new Arqueiro("Legolis");
+
+        g.status();
+        m.status();
+        a.status();
+
+        g.atacar(m);
+        m.usarHabilidadeEspecial();
+        a.usarHabilidadeEspecial();
+
+        g.status();
+        m.status();
+        a.status();
+    }
+}
