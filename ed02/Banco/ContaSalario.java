ef05abbf5c40cc2f0a959274d539c9cb8a61331c
Luiz Henrique <50057405+lhzluiz@users.noreply.github.com>
2025-05-16 20:45:47 -0300

    Update ContaSalario.java

--- ed02/Banco/ContaSalario.java
+++ ed02/Banco/ContaSalario.java
@@ -2,6 +2,6 @@ class ContaSalario extends Conta { //subclasse
     ContaSalario(String cliente, double saldo) { super(cliente, saldo); }
 
     void imprimirExtrato() {
-        System.out.println("[Salário] " + cliente + " - R$" + saldo);
+        System.out.println("[Salário] " + cliente + " - R$" + saldo); //não recebe juros
     }
 }
