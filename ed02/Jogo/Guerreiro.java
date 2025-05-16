909294af93b79bcc5418db11432c4ddd84b38fa8
Luiz Henrique <50057405+lhzluiz@users.noreply.github.com>
2025-05-16 20:51:42 -0300

    Update Guerreiro.java

--- ed02/Jogo/Guerreiro.java
+++ ed02/Jogo/Guerreiro.java
@@ -1,16 +1,16 @@
 class Guerreiro extends Personagem {
     public Guerreiro(String nome) {
-        super(nome, 150, 25);
+        super(nome, 150, 25); 
     }
 
-    @Override
+    @Override //sobreescrita de método
     public void defender(int dano) {
-        super.defender(dano / 2);
+        super.defender(dano / 2); //reduz dano pela metade
     }
 
     @Override
     public void usarHabilidadeEspecial() {
         System.out.println(nome + " usa Golpe Duplo!");
-        ataqueBase *= 2;
+        ataqueBase *= 2; //dobra o ataque base temporariamente
     }
 }
