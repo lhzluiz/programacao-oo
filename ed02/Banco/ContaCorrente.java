fa55a6c4eecfbc6d81e38d5a03737347bc4236d8
Luiz Henrique <50057405+lhzluiz@users.noreply.github.com>
2025-05-16 19:38:13 -0300

    comentado

--- ed02/Banco/ContaCorrente.java
+++ ed02/Banco/ContaCorrente.java
@@ -1,7 +1,7 @@
 class ContaCorrente extends Conta {
     ContaCorrente(String cliente, double saldo) { super(cliente, saldo); }
 
-    void aplicarJurosDiarios() { saldo *= 1.001; }
+    void aplicarJurosDiarios() { saldo *= 0.001; }
 
     void imprimirExtrato() {
         System.out.println("[Corrente] " + cliente + " - R$" + saldo);
