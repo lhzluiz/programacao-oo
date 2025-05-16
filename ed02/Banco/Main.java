b1031f2902fc29b83e1bd3665555b588cea42e7b
Luiz Henrique <50057405+lhzluiz@users.noreply.github.com>
2025-05-16 20:46:59 -0300

    Update Main.java

--- ed02/Banco/Main.java
+++ ed02/Banco/Main.java
@@ -1,19 +1,19 @@
-public class Main {
+public class Main { //teste do sistema
     public static void main(String[] args) {
         Conta cc = new ContaCorrente("Alice", 1000); 
         Conta cp = new ContaPoupanca("Bob", 1500); //cc,cp e cs nome dado para acessar a conta corrente, etc..
         Conta cs = new ContaSalario("Carlos", 1200);
 
-        cc.depositar(200);
-        cp.sacar(100);
-        cs.transferir(cc, 300);
+        cc.depositar(200); //alice deposita 200
+        cp.sacar(100); //bob saca 100
+        cs.transferir(cc, 300); //carlos transfere 300 para alice
 
-        cc.aplicarJurosDiarios();
-        cp.aplicarJurosDiarios();
+        cc.aplicarJurosDiarios(); 
+        cp.aplicarJurosDiarios(); //aplicando juros
         cs.aplicarJurosDiarios();
 
         cc.imprimirExtrato();
-        cp.imprimirExtrato();
+        cp.imprimirExtrato(); //exibindo extratos
         cs.imprimirExtrato();
     }
 }
