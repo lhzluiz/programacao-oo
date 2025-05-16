e2619bad7a9e5f303efbccf34734f75d63a9f715
Luiz Henrique <50057405+lhzluiz@users.noreply.github.com>
2025-05-16 20:40:43 -0300

    Update ContaCorrente.java

--- ed02/Banco/ContaCorrente.java
+++ ed02/Banco/ContaCorrente.java
@@ -1,9 +1,9 @@
-class ContaCorrente extends Conta {
-    ContaCorrente(String cliente, double saldo) { super(cliente, saldo); }
+class ContaCorrente extends Conta { //subclasse que representa uma conta corrente
+    ContaCorrente(String cliente, double saldo) { super(cliente, saldo); } //chama o construtor da classe pai
 
-    void aplicarJurosDiarios() { saldo *= 0.001; }
+    void aplicarJurosDiarios() { saldo *= 0.001; } //aplicando os juros diarios
 
-    void imprimirExtrato() {
+    void imprimirExtrato() { //imprime o extrato
         System.out.println("[Corrente] " + cliente + " - R$" + saldo);
     }
 }
