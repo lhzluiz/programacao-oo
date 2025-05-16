abc49d0da0edca1312eaf3032ed2ca554761929f
Luiz Henrique <50057405+lhzluiz@users.noreply.github.com>
2025-05-16 20:34:14 -0300

    Update Conta.java

--- ed02/Banco/Conta.java
+++ ed02/Banco/Conta.java
@@ -1,17 +1,18 @@
 class Conta { //Classe pai
-    String cliente; //Atributo cliente
-    double saldo; //atributo saldo
+    String cliente; //Atributo cliente (nome)
+    double saldo; //atributo saldo (saldo da conta)
 
-    Conta(String cliente, double saldo) { //Construtor
+    Conta(String cliente, double saldo) { //Construtor da classe conta
         this.cliente = cliente;
         this.saldo = saldo;
     }
 
-    void depositar(double valor) { saldo += valor; } 
+    void depositar(double valor) { saldo += valor; } //depositar um valor na conta
 
-    void sacar(double valor) { if (saldo >= valor) saldo -= valor; } //se o saldo for maior ou igual ao valor soliticado, realizado a operação.
+    void sacar(double valor) { if (saldo >= valor) saldo -= valor; } //se o saldo for maior ou igual ao valor soliticado, 
+    // realizado a operação.
 
-    void transferir(Conta destino, double valor) {
+    void transferir(Conta destino, double valor) { //método para transferir um valor de uma conta para outra
         if (saldo >= valor) {    
             saldo -= valor;
             destino.saldo += valor;                                    //METODOS
@@ -20,7 +21,7 @@ class Conta { //Classe pai
 
     void aplicarJurosDiarios() {} // não há nada definido "dentro", pois será usado posteriormente nas subclasses
 
-    void imprimirExtrato() {
+    void imprimirExtrato() { //imprimir o extrato da conta
         System.out.println("Cliente: " + cliente + ", Saldo: R$" + saldo);
     }
 }
