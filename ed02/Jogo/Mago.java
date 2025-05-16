43876f9976f82a34ff1068fefa0c16bae8bdff9c
Luiz Henrique <50057405+lhzluiz@users.noreply.github.com>
2025-05-16 20:52:25 -0300

    Update Mago.java

--- ed02/Jogo/Mago.java
+++ ed02/Jogo/Mago.java
@@ -1,7 +1,7 @@
 class Mago extends Personagem { // criando a subclasse Mago
                                 // a palavra extends é a que define o uso da herança da classe pai (Personagem)
     public Mago(String nome) {
-        super(nome, 100, 30);
+        super(nome, 100, 30); // Ataque alto, defesa fraca, lança bola de fogo
     }
 
     @Override
