7cb20197323f8dabfb402d82f1384f39f10915d8
Luiz Henrique <50057405+lhzluiz@users.noreply.github.com>
2025-06-04 14:04:24 -0300

    Create Imprimivel.java

---
+++ ed03/GraficaOnline/Imprimivel.java
@@ -0,0 +1,5 @@
+package GraficaOnline;  
+
+public interface Imprimivel {  // Interface Imprimivel define um contrato de impressão
+    void imprimir();  // Método obrigatório para todas as classes que implementarem
+}
