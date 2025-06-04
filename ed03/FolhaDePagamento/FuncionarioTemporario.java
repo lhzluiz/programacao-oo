fbbfe908938699d1a163f8ee617a36a88995533b
Luiz Henrique <50057405+lhzluiz@users.noreply.github.com>
2025-06-04 13:56:09 -0300

    Create FuncionarioTemporario.java

---
+++ ed03/FolhaDePagamento/FuncionarioTemporario.java
@@ -0,0 +1,15 @@
+package FolhaDePagamento;  
+
+public class FuncionarioTemporario extends Funcionario {  // subclasse Funcionario
+
+    private static final double DESCONTO_TEMPORARIO = 100.0;  // desconto fixo de R$100,00
+
+    public FuncionarioTemporario(String nome, double salarioBase) {  // construtor chamando a superclasse
+        super(nome, salarioBase);
+    }
+
+    @Override
+    public double calcularSalario() {  // implementação do cálculo de salário (polimorfismo visto na auula)
+        return salarioBase - DESCONTO_TEMPORARIO;
+    }
+}
