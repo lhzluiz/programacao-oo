b7383ed06e937757bf3983e2d8b29d9aa3637049
Luiz Henrique <50057405+lhzluiz@users.noreply.github.com>
2025-06-18 08:17:04 -0300

    Create Main.java

---
+++ ed04/Main.java
@@ -0,0 +1,6 @@
+public class Main { // classe pai, proncipal
+    public static void main(String[] args) {
+        ContatoController controller = new ContatoController(); // instancia o controller
+        controller.executar(); // inicia
+    }
+}
