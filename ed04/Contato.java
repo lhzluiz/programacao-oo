1f7b8e8a6371bd332ae23da23ba76d1eed3097d2
Luiz Henrique <50057405+lhzluiz@users.noreply.github.com>
2025-06-18 08:47:49 -0300

    Create Contato.java

---
+++ ed04/Contato.java
@@ -0,0 +1,6 @@
+public interface Contato { // define os métodos obrigatórios
+    String getNome(); // nome
+    String getEmail(); // email            estou retornando todos.
+    String getTelefone(); // telefone
+    String toCSV(); // formato CSV
+}
